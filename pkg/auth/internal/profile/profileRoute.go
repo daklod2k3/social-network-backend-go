165090221a57b002b7c0701ffaa4ef7c036a40b6
daklod2k3 <trandacloc123@gmail.com>
2025-01-22 18:44:07 +0700

    remove pkg auth form this repo and add it to new submodule

--- pkg/auth/internal/profile/profileRoute.go
+++
@@ -1,9 +0,0 @@
-package profile
-
-import "github.com/gin-gonic/gin"
-
-func (s *controller) ApplyRoute(r *gin.RouterGroup, authorized *gin.RouterGroup) {
-	path := authorized.Group("/profile")
-	path.POST("", s.CreateHdl)
-	path.GET("", s.GetHdl)
-}
